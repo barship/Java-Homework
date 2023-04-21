f40d38a03041f09b24f6f3d67f7ec7d9654aeee6
barship <49543814+barship@users.noreply.github.com>
2023-04-21 11:04:52 +0800

    Add files via upload

---
+++ ShoppingManagementSystem.java
@@ -0,0 +1,22 @@
+public class ShoppingManagementSystem {
+    public static void main(String[] args) {
+        for (int i=0;i<39;i++) System.out.print("* ");
+        System.out.println("\n");
+
+        for (int i=0;i<30;i++) System.out.print(" ");
+        System.out.println("欢迎使用购物管理系统1.0版");
+        System.out.println("");
+
+        for (int i=0;i<34;i++) System.out.print(" ");
+        System.out.println("1. 登 录 系 统");
+        System.out.println("");
+
+        for (int i=0;i<34;i++) System.out.print(" ");
+        System.out.println("2. 退 出");
+        System.out.println("");
+
+        for (int i=0;i<39;i++) System.out.print("* ");
+        System.out.println("");
+        System.out.println("请选择数字(1~2):");
+    }
+}
